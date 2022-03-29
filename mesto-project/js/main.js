a311e00da91d3ff98214929e8240dca756c61340
Savva <osv1313006@gmail.com>
2022-03-29 21:27:17 +0300

    same

--- mesto-project/js/main.js
+++ mesto-project/js/main.js
@@ -1,19 +1,21 @@
+// Edit
 const editBtn = document.querySelector(".btn_btn_edit");
-const addBtn = document.querySelector(".btn_btn_add");
 const closeBtn = document.querySelector(".popup__close");
 const popupEdit = document.querySelector(".popup_edit");
-const popupAdd = document.querySelector(".popup_add");
-const form = document.querySelector(".form");
-const nameInput = popupEdit.querySelector(".form__input_name");
-const roleInput = popupEdit.querySelector(".form__input_role");
+const formEdit = document.querySelector(".form-edit");
+const nameInput = document.querySelector(".form__input_name");
+const roleInput = document.querySelector(".form__input_role");
 const formSaveBtn = document.querySelector(".btn_btn_save");
+// ProfileInfo
 const profileHeading = document.querySelector(".profile__heading");
 const profileSubTitle = document.querySelector(".profile__subtitle");
-const cards = document.querySelector(".hero").cloneNode(true);
-const card = document.querySelector(".hero__item");
-const likeBtns = document.querySelectorAll(".btn_btn_like");
+// AddPhoto
+const cards = document.querySelector(".hero");
+const popupAdd = document.querySelector(".popup_add");
+const addBtn = document.querySelector(".btn_btn_add");
+const cardTemplate = document.querySelector("#card").content;
 const popups = document.querySelectorAll(".popup");
-
+const formAdd = document.querySelector(".form-add");
 const initialCards = [
   {
     name: "Архыз",
@@ -41,6 +43,8 @@ const initialCards = [
   },
 ];
 
+// openPopup && closePopup
+
 function openPopup(popup) {
   popup.classList.add("popup_opened");
 }
@@ -63,19 +67,20 @@ closeBtn.addEventListener("click", () => {
   closePopup(popupEdit);
 });
 
+closeBtn.addEventListener("click", () => {
+  closePopup(popupAdd);
+});
+
+// editBtn
+
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
   profileHeading.textContent = nameInput.value;
   profileSubTitle.textContent = roleInput.value;
   closePopup(popupEdit);
 }
 
-form.addEventListener("submit", formSubmitHandler);
-
-// initialCards.forEach((card) => {
-//   cards.prepend(createCard(card.name, card.link));
-// });
+// Likes && Utils
 
 popups.forEach((popup) => {
   popup.addEventListener("click", (evt) => {
@@ -88,8 +93,43 @@ popups.forEach((popup) => {
   });
 });
 
-likeBtns.forEach((likeBtn) => {
+initialCards.forEach((card) => {
+  cards.prepend(createCard(card.name, card.link));
+});
+
+// addBtn
+
+const cardLink = document.querySelector(".form__input_link");
+const cardTitle = document.querySelector(".form__input_title");
+
+function createCard(name, link) {
+  const cardElement = cardTemplate.querySelector(".hero__item").cloneNode(true);
+  const cardName = cardElement.querySelector(".hero__heading");
+  const cardPhoto = cardElement.querySelector(".hero__pic");
+  const likeBtn = cardElement.querySelector(".btn_btn_like");
+  const cardDel = cardElement.querySelector(".hero__del");
+
+  cardName.innerText = name;
+  cardPhoto.alt = name;
+  cardPhoto.src = link;
+
   likeBtn.addEventListener("click", (evt) => {
     evt.target.classList.toggle("btn_btn_like-active");
   });
-});
+  cardDel.addEventListener("click", () => {
+    const cardItem = cardDel.closest(".hero__item");
+    cardItem.remove();
+  });
+
+  return cardElement;
+}
+
+function formSubmitHandlerAdd(evt) {
+  evt.preventDefault();
+  cards.prepend(createCard(cardTitle.value, cardLink.value));
+  closePopup(popupAdd);
+  formAdd.reset();
+}
+
+formEdit.addEventListener("submit", formSubmitHandler);
+formAdd.addEventListener("submit", formSubmitHandlerAdd);
